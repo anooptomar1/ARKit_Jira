bacef57be92cd972e42c6d986578962cb4b329bf
Zach 'Leif' Eriksen <zeriksen@clientresourcesinc.com>
2018-07-20 10:55:32 -0500

    Set scene to default and configure images to track

--- JiraTicketAR/ViewController.swift
+++ JiraTicketAR/ViewController.swift
@@ -23,19 +23,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
         // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.scene = SCNScene()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
 
+        guard let images = ARReferenceImage.referenceImages(inGroupNamed: "Images", bundle: Bundle.main) else {
+            fatalError("No Images (Make sure the folder name is `Images`)")
+        }
+        configuration.trackingImages = images
+        configuration.maximumNumberOfTrackedImages = images.count
+        
         // Run the view's session
         sceneView.session.run(configuration)
     }
